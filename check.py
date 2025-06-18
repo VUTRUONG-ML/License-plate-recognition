b8f7c30c93977378078a74dc37b47f4dea233c14
LE BAR <letruong8017@gmail.com>
2025-06-18 10:58:12 +0700

    Update file system

--- check.py
+++
@@ -1,5 +0,0 @@
-
-if __name__ == '__main__':
-    # Khởi tạo bảng biensoxedangky nếu chưa có
-    mongo_db.initialize_biensoxedangky()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
--- sendvideo.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-with open("E:/PythonSource/NhanDangBienSoXe/Data/4.mp4", "rb") as f:
-    files = {'video': f}
-    res = requests.post("http://127.0.0.1:5000/detect_license_plate", files=files)
-
-print(res.json())
